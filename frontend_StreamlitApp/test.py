e53185b85d8c6f0b6f095af877c389ba22d3e638
areeshanabila <areeshnbila@gmail.com>
2026-06-27 18:48:46 +0800

    update pkl

--- frontend_StreamlitApp/test.py
+++ frontend_StreamlitApp/test.py
@@ -5,7 +5,7 @@ vec = joblib.load("best_vectorizer.pkl")
 print("Classes:", model.classes_)
 
 # Check "hate" and "excellent"
-for word in ["hate", "excellent", "worst", "great"]:
+for word in ["hate", "excellent", "worst", "great","interesting"]:
     if word in vec.vocabulary_:
         idx = vec.vocabulary_[word]
         print(f"{word}: {model.coef_[0][idx]:.4f}")
